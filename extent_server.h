53dfba47230dedb19d2301b8f29321eddf47f7b1
Neil Forrester <nforrest@mit.edu>
2012-02-23 23:37:40 -0500

    more whitespace changes

--- extent_server.h
+++ extent_server.h
@@ -9,13 +9,13 @@
 
 class extent_server {
 
- public:
-  extent_server();
+	public:
+		extent_server();
 
-  int put(extent_protocol::extentid_t id, std::string, int &);
-  int get(extent_protocol::extentid_t id, std::string &);
-  int getattr(extent_protocol::extentid_t id, extent_protocol::attr &);
-  int remove(extent_protocol::extentid_t id, int &);
+		int put(extent_protocol::extentid_t id, std::string, int &);
+		int get(extent_protocol::extentid_t id, std::string &);
+		int getattr(extent_protocol::extentid_t id, extent_protocol::attr &);
+		int remove(extent_protocol::extentid_t id, int &);
 };
 
 #endif 
