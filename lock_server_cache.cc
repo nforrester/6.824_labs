ecd0fd65baf6a03e5edf23fd7485705b22f85288
Neil Forrester <nforrest@mit.edu>
2012-03-16 01:51:48 -0400

    Made indentation acceptable

--- lock_server_cache.cc
+++ lock_server_cache.cc
@@ -10,31 +10,23 @@
 #include "tprintf.h"
 
 
-lock_server_cache::lock_server_cache()
-{
+lock_server_cache::lock_server_cache() {
 }
 
 
-int lock_server_cache::acquire(lock_protocol::lockid_t lid, std::string id, 
-                               int &)
-{
-  lock_protocol::status ret = lock_protocol::OK;
-  return ret;
+int lock_server_cache::acquire(lock_protocol::lockid_t lid, std::string id, int &) {
+	lock_protocol::status ret = lock_protocol::OK;
+	return ret;
 }
 
-int 
-lock_server_cache::release(lock_protocol::lockid_t lid, std::string id, 
-         int &r)
-{
-  lock_protocol::status ret = lock_protocol::OK;
-  return ret;
+int lock_server_cache::release(lock_protocol::lockid_t lid, std::string id, int &r) {
+	lock_protocol::status ret = lock_protocol::OK;
+	return ret;
 }
 
-lock_protocol::status
-lock_server_cache::stat(lock_protocol::lockid_t lid, int &r)
-{
-  tprintf("stat request\n");
-  r = nacquire;
-  return lock_protocol::OK;
+lock_protocol::status lock_server_cache::stat(lock_protocol::lockid_t lid, int &r) {
+	tprintf("stat request\n");
+	r = nacquire;
+	return lock_protocol::OK;
 }
 
