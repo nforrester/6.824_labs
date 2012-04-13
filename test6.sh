3d2d6cee78d300fc766a7e7143952013c7f28b98
Neil Forrester <nforrest@mit.edu>
2012-04-12 21:07:09 -0400

    added test

---
+++ test6.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+make
+
+echo ""
+echo "---------- BEGINNING TESTS 0-7 ----------"
+echo ""
+./rsm_tester.pl 0 1 2 3 4 5 6 7
+echo ""
+echo "----------- ENDING TESTS 0-7 ------------"
+echo ""
+
+killall lock_server
+rm -f *.log
